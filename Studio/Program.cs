2524363d1af51788ace954d6089b9e608de1e511
saranell <76405224+saranell@users.noreply.github.com>
2022-08-17 18:20:14 -0500

    Updated studio

--- Studio/Program.cs
+++ Studio/Program.cs
@@ -1,25 +1,36 @@
 ﻿//Studio: Counting Characters
 //Write a program to count the number of times each character occurs in a string and print the results.
-Console.WriteLine("Hello");
+using System;
+using System.Collections.Generic;
+
 namespace CountingCharacters
 {
     public class Program
-{
-    static void Main(string[] args)
+    {
+        public static void Main(string[] args)
         {
-    
-            Dictionary<char, int> characterCount = new();
-            string text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\nNunc accumsan sem ut ligula scelerisque sollicitudin.\nUt at sagittis augue. Praesent quis rhoncus justo.\nAliquam erat volutpat. Donec sit amet suscipit metus, non lobortis massa.\nVestibulum augue ex, dapibus ac suscipit vel, volutpat eget massa.\nDonec nec velit non ligula efficitur luctus.";
 
-            
-            int count = 0;
+            string text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\nNunc accumsan sem ut ligula scelerisque sollicitudin.\nUt at sagittis augue. Praesent quis rhoncus justo.\nAliquam erat volutpat. Donec sit amet suscipit metus, non lobortis massa.\nVestibulum augue ex, dapibus ac suscipit vel, volutpat eget massa.\nDonec nec velit non ligula efficitur luctus.";
+            Dictionary<char, int> count = new();
+
             foreach (char letter in text)
             {
-                count++;
+                if (!count.Keys.Contains(letter))
+                {
+                    count.Add(letter, 1);
+                }
+                else
+                {
+                    count[letter]++;
+                }
+
+
+                foreach (KeyValuePair<char, int> count in count)
+                {
+                    Console.WriteLine(count.Key + ": " + count.Value);
+                }
             }
-
-}
-
+        }
     }
 
 }
