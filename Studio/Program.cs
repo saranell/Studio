333a8a99b0a141418d520ebb033510fedc47aba1
saranell <76405224+saranell@users.noreply.github.com>
2022-08-22 13:32:29 -0500

    Added super bonus from class example

--- Studio/Program.cs
+++ Studio/Program.cs
@@ -1,4 +1,4 @@
-﻿//Studio: Counting Characters
+﻿//3.8 Studio
 //Write a program to count the number of times each character occurs in a string and print the results.
 using System;
 using System.Collections.Generic;
@@ -9,29 +9,39 @@ namespace CountingCharacters
     {
         public static void Main(string[] args)
         {
+            //Super Bonus: Read the string in from a file.
+            //Solution: Create text file & import the following class...
+            //string exampleString = System.IO.File.ReadAllText( --> "local path to text file goes here" <-- );
 
-            string text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\nNunc accumsan sem ut ligula scelerisque sollicitudin.\nUt at sagittis augue. Praesent quis rhoncus justo.\nAliquam erat volutpat. Donec sit amet suscipit metus, non lobortis massa.\nVestibulum augue ex, dapibus ac suscipit vel, volutpat eget massa.\nDonec nec velit non ligula efficitur luctus.";
-            Dictionary<char, int> count = new();
+            //string exampleString = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\nNunc accumsan sem ut ligula scelerisque sollicitudin.\nUt at sagittis augue. Praesent quis rhoncus justo.\nAliquam erat volutpat. Donec sit amet suscipit metus, non lobortis massa.\nVestibulum augue ex, dapibus ac suscipit vel, volutpat eget massa.\nDonec nec velit non ligula efficitur luctus.";
+            //Bonus: Prompt the user to enter the string in the command line.
+            Console.WriteLine("Enter some text to count the characters: ");
+            string exampleString = Console.ReadLine();
 
-            foreach (char letter in text)
+            //Bonus: Make the character counts case-insensitive.
+            char[] characters = exampleString.ToLower().ToCharArray();
+
+            Dictionary<char, int> counts = new Dictionary<char, int>();
+
+            foreach (char c in characters)
             {
-                if (!count.Keys.Contains(letter))
+                if (!counts.Keys.Contains(c))
                 {
-                    count.Add(letter, 1);
+                    counts.Add(c, 1);
                 }
                 else
                 {
-                    count[letter]++;
-                }
-
-
-                foreach (KeyValuePair<char, int> count in count)
-                {
-                    Console.WriteLine(count.Key + ": " + count.Value);
+                    counts[c]++;
                 }
             }
+
+                foreach (KeyValuePair<char, int> letter in counts)
+                {
+                    Console.WriteLine(letter.Key + ": " + letter.Value);
+                }
+
+                //Console.ReadLine();
         }
     }
-
 }
 
